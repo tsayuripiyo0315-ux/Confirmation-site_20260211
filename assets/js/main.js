050f7c0ba8227884509bc77d47a39b05849f7b0f
takano sayuri <takanosayuri@MacBook-Air-SAYU.local>
2026-02-17 17:39:02 +0900

    feat: ローディング待ち時間変更

--- assets/js/main.js
+++ assets/js/main.js
@@ -52,7 +52,7 @@ $(function () {
             if (!isWindowLoaded) {
                 onImageReady();
             }
-        }, 3000);
+        }, 2500);
 
         const $loadingImg = $('.loading-img');
         if ($loadingImg.length && $loadingImg[0].complete) {
